88366801da849ba666572d78775d9aaaedada4db
Huy Tinh <170064219+HuyTinh@users.noreply.github.com>
2024-10-23 22:05:39 +0700

    update add on icon.

--- src/static/layout/index.ts
+++ src/static/layout/index.ts
@@ -11,7 +11,7 @@ export const BaseHTML = ({ children }: elements.Children) => {
                 <meta name="description" content="Ladi Server" />
                 <script src="https://cdn.tailwindcss.com"></script>
                 <script src="https://unpkg.com/htmx.org@2.0.3" integrity="sha384-0895/pl2MU10Hqc6jd4RvrthNlDiE9U1tWmX7WRESftEDRosgxNsQG/Ze9YMRzHq" crossorigin="anonymous"></script>
-
+                <link rel="icon" type="image/x-icon" href="https://cdn-icons-png.flaticon.com/512/1499/1499993.png">
                 <title>Ladi Server</title>
             </head>
                 ${children}
